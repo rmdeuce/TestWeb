614a84f7980784bf58e9d69258c6ab8f4d1a11d6
rmdeuce <rmdeuce@gmail.com>
2022-12-02 18:23:44 +1000

    Добавьте файлы проекта.

---
+++ Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+    /// <summary>
+    /// Человек
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
